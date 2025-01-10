ca91c4353cfb585d1b35623b8ffe2b60b21cfa77
ComicalTNM <54879407+ComicalTNM@users.noreply.github.com>
2025-01-10 13:21:38 -0500

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import connectDB from './db'; // Import the DB connection function
 import authRoutes from './routes/authRoutes';
 import config from './config';
+import dotenv from 'dotenv';
 
 const app = express();
 app.use(express.json()); // Middleware to parse JSON
+dotenv.config();
 
 // Connect to MongoDB
 connectDB();
